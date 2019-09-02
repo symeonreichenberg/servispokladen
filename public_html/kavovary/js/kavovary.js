6641885e0e09f784d8ec2abb5154b33d30b6a029
Vojtech Basl <vojtech.basl@mailprofiler.com>
2019-09-02 15:08:10 +0200

    kavovary || HTML include || sample products

--- public_html/kavovary/js/kavovary.js
+++ public_html/kavovary/js/kavovary.js
@@ -87,3 +87,30 @@ card.getBody().innerHTML = "Revoluční 185/43<br>466 01 Jablonec nad Nisou";
 var marker = new SMap.Marker(center, "myMarker");
 marker.decorate(SMap.Marker.Feature.Card, card);
 layer.addMarker(marker);
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      } 
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
